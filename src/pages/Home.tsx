82f9dc78ad3d5c8702af0226bed77477a9194558
ch4tan <80721563+ch4tan@users.noreply.github.com>
2025-07-05 14:26:02 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="flex flex-col h-full w-full text-center justify-between items-center bg-[#EA925E] border-b-2 border-t-2 text-white gap-2 md:gap-0">
+        <div className="flex flex-col h-full w-full text-center justify-between items-center bg-[#fdba3e] border-b-2 border-t-2 text-white gap-2 md:gap-0">
             <div className="flex flex-col justify-center items-center w-80 gap-3 md:gap-0 md:w-full pt-10">
                 <h1 className="font-bitcount md:text-5xl md:pt-10">MEOWFLOW B64 RECURSIVE ENCODER/DECODER</h1>
                 <h2 className="flex md:text-xl font-playfair md:w-[50rem] md:pt-5 font-bold">
